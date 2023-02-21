af831f9f2854e205b8a3a73e86e08858f64427c3
Azad <azad.hesso90@gmail.com>
2023-02-21 19:13:00 +0300

    Made getters and setters for the message

--- Console chat-App/Console chat-App/Message.cpp
+++ Console chat-App/Console chat-App/Message.cpp
@@ -2,3 +2,33 @@
 
 Message::Message(const std::string& from, const std::string& to, const std::string msg)
     : _from(from), _to(to), _msg(msg) {}
+
+const std::string& Message::getFrom() const
+{
+    return _from;
+}
+
+void Message::setFrom(const std::string& from)
+{
+    _from = from;
+}
+
+const std::string& Message::getTo() const
+{
+    return _to;
+}
+
+void Message::setTo(const std::string& to)
+{
+    _to = to;
+}
+
+const std::string& Message::getMsg() const
+{
+    return _msg;
+}
+
+void Message::setMsg(const std::string& msg)
+{
+    _msg = msg;
+}
--- Console chat-App/Console chat-App/Message.h
+++ Console chat-App/Console chat-App/Message.h
@@ -6,6 +6,14 @@ class Message
 public:
 	Message(const std::string& from, const std::string& to, const std::string msg);
 
+	const std::string& getFrom() const;
+	void setFrom(const std::string& from);
+
+	const std::string& getTo() const;
+	void setTo(const std::string& to);
+
+	const std::string& getMsg() const;
+	void setMsg(const std::string& msg);
 
 private:
 	std::string _from;
