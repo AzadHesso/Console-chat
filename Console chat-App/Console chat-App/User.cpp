1d6d5ed888bf0d09a0b6f01e38754986389c0b80
Azad <azad.hesso90@gmail.com>
2023-02-21 18:31:18 +0300

    Made getters and setters for the user field

--- Console chat-App/Console chat-App/User.cpp
+++ Console chat-App/Console chat-App/User.cpp
@@ -2,3 +2,33 @@
 
 User::User(const std::string& name, const std::string& login, const std::string& password)
           : _name(name), _login(login), _password(password) {}
+
+const std::string& User::getName() const
+{
+    return _name;
+}
+
+void User::setName(const std::string& name)
+{
+    _name = name;
+}
+
+const std::string& User::getLogin() const
+{
+    return _login;
+}
+
+void User::setLogin(const std::string& login)
+{
+    _login = login;
+}
+
+const std::string& User::getPassword() const
+{
+    return _password;
+}
+
+void User::setPassowrd(const std::string& passowrd)
+{
+    _password = passowrd;
+}
--- Console chat-App/Console chat-App/User.h
+++ Console chat-App/Console chat-App/User.h
@@ -6,12 +6,18 @@ class User
 public:
 	User(const std::string& name, const std::string& login, const std::string& password);
 
+	const std::string& getName() const;
+	void setName(const std::string& name);
 
+	const std::string& getLogin() const;
+	void setLogin(const std::string& login);
 
+	const std::string& getPassword() const;
+	void setPassowrd(const std::string& passowrd);
 
 
 private:
 	std::string _name;
-	const std::string _login;
+    const std::string _login;
 	std::string _password;
 };
