a4906a06aaa48aab716de7fcdc40811b82ff5bf6
Azad <azad.hesso90@gmail.com>
2023-02-23 17:18:58 +0300

    realization show chat

--- Console chat-App/Console chat-App/Chat.cpp
+++ Console chat-App/Console chat-App/Chat.cpp
@@ -70,6 +70,34 @@ void Chat::signUp()
 
 }
 
+void Chat::showChat() const
+{
+	std::string from;
+	std::string to;
+
+	std::cout << " Chat " << std::endl;
+
+	for (auto& message : _messageArr)
+	{
+		if (_currentUser->getLogin() == message.getFrom() || _currentUser->getLogin() == message.getTo() || message.getTo() == "all")
+		{
+			from = (_currentUser->getLogin() == message.getFrom()) ? "Me" : getUserByLogin(message.getFrom())->getName();
+
+			if (message.getTo() == "all")
+			{
+				to = "all";
+			}
+			else
+			{
+				to = (_currentUser->getLogin() == message.getTo()) ? "Me" : getUserByLogin(message.getTo())->getName();
+			}
+
+			std::cout << "Message from " << from << " to " << to << std::endl;
+			std::cout << "Msg: " << message.getMsg() << std::endl;
+		}
+	}
+}
+
 std::vector<User>& Chat::getAllUsers()
 {
 	return _userArr;
--- Console chat-App/Console chat-App/Chat.h
+++ Console chat-App/Console chat-App/Chat.h
@@ -1,1 +1,38 @@
 #pragma once
+#include "Message.h"
+#include <memory>
+#include <vector>
+#include <exception>
+
+class Chat
+{
+public:
+	void start();
+	bool isChatWork() const;
+	std::shared_ptr<User> getCurrentUser() const;
+	void showLoginMenu();
+	void showUserMenu();
+	void login();
+	void signUp();
+	void showChat() const;
+	void showAllUsersName()const;
+	void addMessage();
+	std::vector<User>& getAllUsers();
+	std::vector<Message>& getAllMessages();
+	std::shared_ptr<User> getUserByLogin(const std::string& login) const;
+
+
+private:
+	bool _isChatWork = false;
+	std::vector<User> _userArr;
+	std::vector<Message> _messageArr;
+	std::shared_ptr<User> _currentUser = nullptr;
+};
+
+struct LoginException : public std::exception
+{
+	const char* what() const noexcept override
+	{
+		return "Error: Login is busy!";
+	}
+};
